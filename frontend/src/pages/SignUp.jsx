51e8da7ec64e28b944a4f033031a69e12c99051b
Joshua Bamidele <exploits4real@gmail.com>
2024-06-04 10:24:39 +0100

    pseudo Login

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,6 +2,30 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <section>
+
+<div class="container">
+    <div class="login-container">
+      <form>
+        <h2 className='text-2xl text-center font-semibold '>Login</h2>
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input type="text" id="name" />
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" id="email" />
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <button >Login</button>
+      </form>
+    </div>
+  </div>
+
+        
+    </section>
   )
 }
