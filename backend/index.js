2a4857fc376874cd718299faf3a39c6761645d54
Joshua Bamidele <exploits4real@gmail.com>
2024-06-05 23:59:09 +0100

    Creating api routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRouter from './routes/userRoute.js';
 
 dotenv.config()
 
@@ -11,6 +12,13 @@ mongoose
     })
 
 const app = express ()
+
+
+// Routes
+app.use("/api/user", userRouter);
+
+
+
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
