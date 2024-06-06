9df2bfebb656221a7408bf002691034f4442bd5d
Joshua Bamidele <exploits4real@gmail.com>
2024-06-07 00:07:02 +0100

    complete sighup backend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,7 @@
 import User from '../models/userModel.js'
 
 
-export const signup = (req, res)=>{
+export const signup = async (req, res)=>{
         const { name, email, password , avatar, phone, address, nationality, twitter, linkedin, instagram, facebook, youtube, biblical_stories, science_fiction, mystery_thriller, historical_fiction, adventure, biography, children_stories, literacy_fiction, humor, drama, non_fiction, poetry } = req.body
         if ( !name || !email || !password || name === '' || email === '' || password === '' ) {
                 return res.status(400).json("All fields are required")
@@ -9,28 +9,31 @@ export const signup = (req, res)=>{
 
         const newUser = new User({
                 name,
-                 email,
-                  password,
-                   avatar,
-                    phone, 
-                    address,
-                     nationality, 
-                     twitter, 
-                     linkedin, 
-                     instagram,
-                      facebook,
-                       youtube,
-                        biblical_stories,
-                         science_fiction,
-                          mystery_thriller,
-                           historical_fiction,
-                            adventure,
-                             biography,
-                              children_stories,
-                               literacy_fiction,
-                                humor,
-                                 drama,
-                                  non_fiction,
-                                   poetry   
-        })
+                email,
+                password,
+                avatar,
+                phone, 
+                address,
+                nationality, 
+                twitter, 
+                linkedin, 
+                instagram,
+                facebook,
+                youtube,
+                biblical_stories,
+                science_fiction,
+                mystery_thriller,
+                historical_fiction,
+                adventure,
+                biography,
+                children_stories,
+                literacy_fiction,
+                humor,
+                drama,
+                non_fiction,
+                poetry,   
+        });
+
+        await newUser.save();
+        res.json( "Signup Successfully")
 };
