6165af1624c59abc183edc53fb7fbe5521b369ba
Joshua Bamidele <exploits4real@gmail.com>
2024-06-06 23:59:05 +0100

    auth signup

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,5 +2,35 @@ import User from '../models/userModel.js'
 
 
 export const signup = (req, res)=>{
-        console.log(req.body)
-}
+        const { name, email, password , avatar, phone, address, nationality, twitter, linkedin, instagram, facebook, youtube, biblical_stories, science_fiction, mystery_thriller, historical_fiction, adventure, biography, children_stories, literacy_fiction, humor, drama, non_fiction, poetry } = req.body
+        if ( !name || !email || !password || name === '' || email === '' || password === '' ) {
+                return res.status(400).json("All fields are required")
+        }
+
+        const newUser = new User({
+                name,
+                 email,
+                  password,
+                   avatar,
+                    phone, 
+                    address,
+                     nationality, 
+                     twitter, 
+                     linkedin, 
+                     instagram,
+                      facebook,
+                       youtube,
+                        biblical_stories,
+                         science_fiction,
+                          mystery_thriller,
+                           historical_fiction,
+                            adventure,
+                             biography,
+                              children_stories,
+                               literacy_fiction,
+                                humor,
+                                 drama,
+                                  non_fiction,
+                                   poetry   
+        })
+};
