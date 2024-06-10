34523023cfe2c8dee024cd358ca37638a1d04c1b
Joshua Bamidele <exploits4real@gmail.com>
2024-06-10 23:53:26 +0100

    Signin

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel.js'
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 import { errorHandler } from '../utils/error.js';
 
 
@@ -48,3 +49,33 @@ export const signup = async (req, res, next)=>{
                 next(error)
         }
 };
+
+export const signin = async () => {
+        const { email, password } = req.body;
+
+        if (!email || !password || email === '' || password === ''){
+                next(errorHandler(400, 'All fields are required'))
+        }
+
+        try {
+                const validUser = await User.findOne({email })
+                if(!validUser){
+                        next(errorHandler(404, 'User not form'));
+                }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+                if (!validPassword){
+                        next(errorHandler(400, 'Wrong credentials'))
+                }
+
+                const token =jwt.sign(
+                        {id:validUser._id},
+                        process.env.JWT_SECRET);
+                        // {expiresIn:'1h'}  for the expiration of the token
+                        res.status(200).cookie('access_token', token, {
+                                httpOnly:true
+                        }).json (validUser)
+                
+        } catch (error) {
+                next(error);
+        }
+}
