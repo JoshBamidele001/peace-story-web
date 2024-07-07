0c18db34eee79934a0ba6702a30fc675901a3823
Joshua Bamidele <exploits4real@gmail.com>
2024-07-07 18:33:03 +0100

    complete create post functionality

--- frontend/src/AdminOutlets/CreateAStory.jsx
+++ frontend/src/AdminOutlets/CreateAStory.jsx
@@ -5,6 +5,7 @@ import {getDownloadURL, getStorage, ref,  uploadBytesResumable} from 'firebase/s
 import { app } from '../firebase'
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { useNavigate } from 'react-router-dom';
 
 
 
@@ -12,7 +13,11 @@ export default function CreateAStory() {
   const [file, setfile] = useState(null)
   const [imageUploadProgress, setimageUploadProgress] = useState(null)
   const [imageUploadError, setimageUploadError] = useState(null)
+  const [publishError, setpublishError] = useState(null)
   const [formData, setformData] = useState({})
+
+  const navigate = useNavigate()
+ 
   const handleUploadImage = async ()=>{
       try {
         if (!file){
@@ -52,6 +57,33 @@ export default function CreateAStory() {
         
       }
   }
+
+  const handleSubmit = async (e)=> {
+    e.preventDefault();
+    try {
+      const res = await fetch('/api/post/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json()
+      if (!res.ok) {
+        setpublishError(data.message)
+        return;
+      }
+    
+      if (res.ok){
+        setpublishError(null)
+        navigate(`/post/${data.slug}`)
+      }
+    } catch (error) {
+      setpublishError('Something went wrong')
+      return
+    }
+  
+  }
   return (
     <div>
       
@@ -60,15 +92,17 @@ export default function CreateAStory() {
             <small>Home- Admin- Create-a-story</small>
       </div>
       
-      <div>
+      <form className='px-1' onSubmit={handleSubmit} >
         <p className='text-center text-2xl font-semibold my-5 '>CREATE A STORY</p>
           <div>
               <label className='pe-3 font-semibold' htmlFor="title"> Title:</label>
-              <input type="text" id='title' className='border-2 rounded-lg w-full p-2' />
+              <input type="text" id='title' className='border-2 rounded-lg w-full p-2'
+              onChange={(e)=> setformData({... formData, title: e.target.value})} />
           </div>
            <div className='mt-5'>
             <label htmlFor="genre" className='font-semibold'>Genre:</label>
-              <select name="genre" class="block w-full px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+              <select name="genre" class="block w-full px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              onChange={(e)=> setformData({... formData, genre: e.target.value})}>
               <option value="uncategorized" class="text-gray-700">Select a genre</option>
               <option value="drama" class="text-gray-700">Drama</option>
               <option value="prose" class="text-gray-700">Prose</option>
@@ -79,8 +113,9 @@ export default function CreateAStory() {
           
 
             <div className='mt-5'>
-              <label htmlFor="categories" className='font-semibold'>Categories:</label>
-            <select name="categories" class="block w-full px-3 py-2 mb-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+              <label htmlFor="category" className='font-semibold'>Categories:</label>
+            <select name="category" class="block w-full px-3 py-2 mb-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            onChange={(e)=> setformData({... formData, category: e.target.value})}>
               <option value="uncategorized" class="text-gray-700">Select a category</option>
               <option value="biblical_stories" class="text-gray-700">Biblical Stories</option>
               <option value="science_fiction" class="text-gray-700">Science Fiction</option>
@@ -91,10 +126,7 @@ export default function CreateAStory() {
               <option value="children_stories" class="text-gray-700">Children Stories</option>
               <option value="literacy_fiction" class="text-gray-700">Literacy Fiction</option>
               <option value="humor" class="text-gray-700">Humor</option>
-              <option value="drama" class="text-gray-700">Drama</option>
               <option value="non_fiction" class="text-gray-700">Non-Fiction</option>
-              <option value="poetry" class="text-gray-700">Poetry</option>
-              <option value="prose" class="text-gray-700">Prose</option>
             </select>
 
             </div>
@@ -127,7 +159,10 @@ export default function CreateAStory() {
 
            <div className='mt-3'>
               <ReactQuill theme="snow" placeholder='Write something...'
-              className='h-72 mb-12 '/>
+              className='h-72 mb-12 '
+              onChange={(value) => {
+                setformData({ ...formData, content: value });
+              }}/>
            </div>
 
            {
@@ -144,7 +179,9 @@ export default function CreateAStory() {
            <button className='w-full bg-blue-950 text-white py-2 rounded-xl my-3'
           >PUBLISH</button>
 
-      </div>
+{publishError && <small className='text-red-600 italic my-5 bg-slate-100  rounded p-2'>{publishError}</small>}
+
+      </form>
       
       </div>
   )
