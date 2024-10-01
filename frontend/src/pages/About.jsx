10dc38fb64672ba46102973d999e8b6d137fb99f
Joshua Bamidele <exploits4real@gmail.com>
2024-10-01 04:31:19 +0100

    Read and About us adjustment

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -16,21 +16,21 @@ export default function About() {
       <div>
       
 
-          {/* the General Overseer */}
+          {/* vission */}
           <div className='w-full bg-purple-100 py-5'>
-           <div className=' mx-5 md:mx-3 lg:mx-20 flex flex-col md:flex md:items-start  md:flex-row lg:flex-row gap-5 justify-between my-5 lg:my-10'>
-               <div className='flex items-center justify-center'>
+           <div className=' mx-5 md:mx-3 lg:mx-20 flex flex-col md:flex md:items-start  md:flex-row lg:flex-row gap-5 justify-around my-5 lg:my-10'>
+               {/* <div className='flex items-center justify-center'>
                 <div className='lg:h-[400px] lg:w-[400px] md:h-[500px] md:w-[200px] h-[250px] w-[250px] rounded-3xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110' 
-                  style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1) , rgba(71,27,80, 0.3), rgba(71,27,80, 0.9)), url('https://firebasestorage.googleapis.com/v0/b/dphospens.appspot.com/o/CREW%20Images%2F_DSC7948.JPG?alt=media&token=57461fe8-02ba-422a-bd3f-e96e0ed7454a')`,
+                  style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1) , rgba(71,27,80, 0.3), rgba(71,27,80, 0.9)), url('https://firebasestorage.googleapis.com/v0/b/dphospens.appspot.com/o/6066776.jpg?alt=media&token=be3f5739-01fa-4e14-9f6e-419f8ea9e6b0')`,
                     backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPositionX: "40%",}}>
                   </div>  
-               </div>
+               </div> */}
           
-                <div className='mx-10  md:mx-0 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-70'> 
-                    <p className='lg:text-3xl text-sm md:text-xl font-semibold'>VISION</p>
-                    <p className='font-semibold'>-FOUNDER</p>
-                    <p className='text-justify lg:my-5 text-sm md:text-base  w-5/6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic suscipit porro, sint, corporis perspiciatis consectetur consequuntur odio autem minima laborum enim praesentium sequi cumque reprehenderit incidunt assumenda dolor numquam, quibusdam laboriosam iure modi optio ullam odit. Libero quasi minus, labore voluptate ratione accusantium culpa perferendis deserunt est laudantium asperiores delectus laboriosam, ullam numquam consequuntur nulla dolore voluptatem repellendus amet obcaecati inventore nostrum non velit? Tempora, est quibusdam minima totam iure nobis explicabo facere rerum cumque aperiam, omnis debitis voluptate modi ipsa deleniti obcaecati odio minus dicta quas quaerat nulla unde architecto? Amet, similique. Illo, vel? Minus dolore commodi dolorem ipsum.</p>
-                    <p className='lg:my-5 text-justify text-sm md:text-base w-5/6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat eos dolorem delectus sint sequi eius. Earum, possimus! Quia cupiditate atque quod sequi quos deleniti beatae at! Consectetur recusandae sapiente neque.</p>
+                <div className='mx-10 max-w-3xl  md:mx-0 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-70'> 
+                    <p className='lg:text-3xl text-sm md:text-xl font-semibold'>VISION STATEMENT</p>
+                    {/* <p className='font-semibold'>-FOUNDER</p> */}
+                    <p className='text-justify lg:my-5 text-sm md:text-base  w-5/6'>Dphospens aspires to become the leading digital platform for literary creation and appreciation, recognized globally for its commitment to empowering writers and enriching readers. </p>
+                    <p className='lg:my-5 text-justify text-sm md:text-base w-5/6'>Our vision is to build a thriving ecosystem where creativity flourishes, knowledge is shared, and individuals are inspired to pursue their passion for writing and reading.</p>
                    
                 </div>
             </div>
@@ -44,20 +44,20 @@ export default function About() {
               <div className='w-full  p-5' style={{ background: "linear-gradient(to left, #d251ee, #000000)"}}>
            <div className=' text-white md:mx-3 lg:mx-20 flex flex-col-reverse md:flex-row md:items-start lg:flex-row justify-around my-5 lg:my-10   '>
                       
-                <div className='  transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-70'> 
-                    <p className='lg:text-3xl font-semibold'>MISSION</p>
-                    <p className='font-semibold'>-C.T.O</p>
-                    <p className='text-justify lg:my-5 text-sm md:text-base w-5/6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic suscipit porro, sint, corporis perspiciatis consectetur consequuntur odio autem minima laborum enim praesentium sequi cumque reprehenderit incidunt assumenda dolor numquam, quibusdam laboriosam iure modi optio ullam odit. Libero quasi minus, labore voluptate ratione accusantium culpa perferendis deserunt est laudantium asperiores delectus laboriosam, ullam numquam consequuntur nulla dolore voluptatem repellendus amet obcaecati inventore nostrum non velit? Tempora, est quibusdam minima totam iure nobis explicabo facere rerum cumque aperiam, omnis debitis voluptate modi ipsa deleniti obcaecati odio minus dicta quas quaerat nulla unde architecto? Amet, similique. Illo, vel? Minus dolore commodi dolorem ipsum.</p>
-                    <p className='lg:my-5 text-justify text-sm md:text-base w-5/6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat eos dolorem delectus sint sequi eius. Earum, possimus! Quia cupiditate atque quod sequi quos deleniti beatae at! Consectetur recusandae sapiente neque.</p>
-                    <p className='lg:my-5 text-justify text-sm md:text-base w-5/6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat eos dolorem delectus sint sequi eius. Earum, possimus! Quia cupiditate atque quod sequi quos deleniti beatae at! Consectetur recusandae sapiente neque.</p>
+                <div className=' max-w-3xl  transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-70'> 
+                    <p className='lg:text-3xl font-semibold'>MISSION STATEMENT</p>
+                    {/* <p className='font-semibold'>-C.T.O</p> */}
+                    <p className='text-justify lg:my-5 text-sm md:text-base w-5/6'>Dphospens is committed to fostering a vibrant and inclusive community for readers and writers, offering a platform that encourages creative expression, knowledge sharing, and personal growth </p>
+                    <p className='lg:my-5 text-justify text-sm md:text-base w-5/6'>Our mission is to inspire, educate, and entertain by providing diverse literary content, writing resources, and interactive tools that empower individuals to share their voices with the world.</p>
+                    {/* <p className='lg:my-5 text-justify text-sm md:text-base w-5/6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat eos dolorem delectus sint sequi eius. Earum, possimus! Quia cupiditate atque quod sequi quos deleniti beatae at! Consectetur recusandae sapiente neque.</p> */}
                 </div>
 
-                <div className='flex items-center justify-center'>
+                {/* <div className='flex items-center justify-center'>
                   <div className='lg:h-[400px] lg:w-[400px] md:h-[500px] md:w-[200px] h-[250px] w-[250px] rounded-3xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110' 
-                  style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1) , rgba(71,27,80, 0.3), rgba(71,27,80, 0.9)), url('https://firebasestorage.googleapis.com/v0/b/dphospens.appspot.com/o/CREW%20Images%2Fgit%20pic.jpg?alt=media&token=48e40c55-420f-4c92-aebc-6595cd4f1d7c')`,
+                  style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1) , rgba(71,27,80, 0.3), rgba(71,27,80, 0.9)), url('https://firebasestorage.googleapis.com/v0/b/dphospens.appspot.com/o/istockphoto-1249889027-612x612.jpg?alt=media&token=7911c7df-1a59-4911-b0eb-349c28aa975a')`,
                     backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPositionX: "40%",}}>
                   </div>  
-                </div>
+                </div> */}
             </div>
 
               </div>
--- frontend/src/pages/Read.jsx
+++ frontend/src/pages/Read.jsx
@@ -6,6 +6,19 @@ import Poetry from '../Genre and Categories/Poetry'
 export default function Read() {
   return (
     <div className='pt-24'>
+
+      <div className=' mx-2 md:mx-10 p-10  grid grid-cols-1 md:grid-cols-2 gap-5 items-center justify-between '>
+        <div>
+          <img src="https://firebasestorage.googleapis.com/v0/b/dphospens.appspot.com/o/Slider%2FDPHOSPen%20slider1_unlock_052109.jpg?alt=media&token=0d296923-30b8-4ba1-93d3-c2265e1fccaa" alt="" />
+        </div>
+        <div className=' max-w-3xl  transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-70'> 
+                    <p className='lg:text-3xl font-semibold'>ENJOY READING</p>
+                    {/* <p className='font-semibold'>-C.T.O</p> */}
+                    <p className='text-justify lg:my-5 text-sm md:text-base w-5/6'>Dphospens is an innovative digital platform designed to bring together readers and writers from around the world </p>
+                    <p className='lg:my-5 text-justify text-sm md:text-base w-5/6'>Our platform serves as a hub for creative expression, knowledge sharing, and personal growth, offering users the tools and community they need to explore and develop their literary talents</p>
+                    <p className='lg:my-5 text-justify text-sm md:text-base w-5/6'>By combining a user-friendly interface with advanced writing and publishing tools, Dphospens aims to become the go-to platform for anyone passionate about writing and reading..</p>
+                </div>
+      </div>
       
       
       <Prose/>
